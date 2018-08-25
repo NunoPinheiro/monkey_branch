fa2f92976f59c7e5d37cf192ce37ead3c4c24502
Nuno Pinheiro <nuno.pinheiro@smarp.com>
2018-08-25 16:00:59 +0300

    Added code to calculate variants. Updated language example

---
+++ src/variant_calculator.js
@@ -0,0 +1,20 @@
+let md5 = require('md5')
+module.exports.calculate_variant = function(user, experiment){
+  // We are going to use the monkeyBranchId of the user if available
+  // Else we revert to the user id
+  let monkeyId = user.monkeyBranchId || user.Id;
+  let hash = md5(monkeyId + experiment.id)
+  // Get a smaller version of the hash and convert it to int
+  // Then we the modulo by 100 to get a percentage were to locate the user
+  let userLocation = parseInt(hash.substring(20), 16) % 100
+
+  //Calculate specific variant for the user
+  let currentPercentage = 0;
+  for(let i in experiment.variants){
+    let currentVariant = experiment.variants[i]
+    currentPercentage += currentVariant.percentage
+    if(userLocation < currentPercentage){
+      return currentVariant
+    }
+  }
+}
---
+++ tests/calculateVariantTests.js
@@ -0,0 +1,108 @@
+var assert = require('assert');
+var calculate_variant = require('../src/variant_calculator.js').calculate_variant
+
+describe('calculate_variant', function() {
+  it('Should return first variant for 100 distribution', function() {
+    let expectedVariantName = "OnlyVariant";
+    let user = {
+      Id: "TestUser",
+    }
+
+    let experiment = {
+      id: "TestId",
+      variants: [
+        {
+          name: expectedVariantName,
+          percentage: 100
+        }
+      ]
+    }
+    let calculatedVariant = calculate_variant(user, experiment)
+    assert.equal(calculatedVariant.name, expectedVariantName)
+  })
+
+  it('Should return second variant for 50/50 distribution', function() {
+    //Calculated percentage for TestUserTestId is 51, so it should fall into second group
+    let expectedVariantName = "secondVariant";
+    let user = {
+      Id: "TestUser",
+    }
+
+    let experiment = {
+      id: "TestId",
+      variants: [
+        {
+          name: "firstVariant",
+          percentage: 50
+        },
+        {
+          name: expectedVariantName,
+          percentage: 50
+        }
+      ]
+    }
+
+    let calculatedVariant = calculate_variant(user, experiment)
+    assert.equal(calculatedVariant.name, expectedVariantName)
+  })
+
+  it('Should return third variant for 30/30/40 distribution', function() {
+    //Calculated percentage for TestUserTestId is 67, so it should fall into the third group
+    let expectedVariantName = "thirdVariant";
+    let user = {
+      Id: "TestUser",
+    }
+
+    let experiment = {
+      id: "TestId",
+      variants: [
+        {
+          name: "firstVariant",
+          percentage: 30
+        },
+        {
+          name: "secondVariant",
+          percentage: 30
+        },
+        {
+          name: expectedVariantName,
+          percentage: 40
+        }
+      ]
+    }
+
+    let calculatedVariant = calculate_variant(user, experiment)
+    assert.equal(calculatedVariant.name, expectedVariantName)
+  })
+
+
+    it('Should return first variant for 70/10/20 distribution', function() {
+      //Calculated percentage for TestUserTestId is 67, so it should fall into the third group
+      let expectedVariantName = "firstVariant";
+      let user = {
+        Id: "TestUser",
+      }
+
+      let experiment = {
+        id: "TestId",
+        variants: [
+          {
+            name: expectedVariantName,
+            percentage: 70
+          },
+          {
+            name: "secondVariant",
+            percentage: 10
+          },
+          {
+            name: "thirdVariant",
+            percentage: 20
+          }
+        ]
+      }
+
+      let calculatedVariant = calculate_variant(user, experiment)
+      assert.equal(calculatedVariant.name, expectedVariantName)
+    })
+
+})
